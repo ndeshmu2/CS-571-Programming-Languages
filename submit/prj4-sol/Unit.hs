67821ea37e6016304bf8a8e77d42fbe394515ea1
Ninad Deshmukh <ndeshmu2@cs.binghamton.edu>
2022-11-16 18:46:22 -0500

    Adding all files of project4

---
+++ submit/prj4-sol/Unit.hs
@@ -0,0 +1,13 @@
+module Unit
+( assertTrue
+, assertFalse
+, assertEq
+) where
+assertTrue msg cond =
+  if cond
+  then putStr ""
+  else do
+    putStr "fail "
+    putStrLn msg
+assertFalse msg cond = assertTrue msg (not cond)
+assertEq msg v1 v2 = assertTrue msg (v1 == v2)
